bc81128f8c79ff28d442e61543ac0169826aa866
wenkai-Yu <2019905327@chd.edu.cn>
2020-07-27 23:09:26 +0800

    Update Numpy.h

--- Numpy.h
+++ Numpy.h
@@ -7,7 +7,8 @@
 * update    	: 2020.07.24
 * upadte	: 2020.07.25
 * update    	: 2020.07.26
-* version	：1.0.3
+* updata	: 2020.07.27
+* version	：1.0.5
 */
 
 #include <cstring>
@@ -18,48 +19,58 @@ using namespace std;
 * 矩阵的类
 * 仿Python--numpy
 */
+
 template <typename Type>
 class Numpy
 {
 private:
-	const string version = "1.0.3";
-	int Rank, Row;
-	Type** Matrix;
-	Type** temp;
-	int temp_Rank;
+    const string version = "1.0.5";
+    int Rank, Row;
+    int temp_Rank;
+    Type** Matrix;
+    Type** temp;
+    
+    string version_() const;
+    Type** getMatrix() const;
+    void creatMatrix();
+    void destroyedMatrix();
+    int getRank() const { return Rank; }
+    int getRow()  const { return Row; }
 
-	string version_() const;
-	Type** getMatrix() const;
-	void creatMatrix();
-	void destroyedMatrix();
+    // 矩阵的运算
+    Type** __dot(const Numpy<Type>*, const Numpy<Type>&);
 
-	// 矩阵的运算
-	Type** __dot(const Numpy<Type>*, const Numpy<Type>&);
-	
 
 public:
-	Numpy();
-	~Numpy();
-	void __version__() const;
+    Numpy();
+    ~Numpy();
+    void __version__() const;
 
-	// 矩阵的显示
-	void shape() const;
-	void info() const;
+    // 矩阵的显示
+    void shape() const;
+    void info() const;
 
-	// 矩阵的创建
-	Type** array(const Type(&arrays)[2][3], int, int);
-	Type** zeros(const int rank, const int row);
-	Type** ones(const int rank, const int row);
+    // 矩阵的创建
+    Type** array(const Type(&arrays)[2][3], int, int);
+    Type** zeros(const int rank, const int row);
+    Type** ones(const int rank, const int row);
 
-	// 矩阵的运算
-	Numpy<Type>& operator += (const Numpy<Type>&);
-	Numpy<Type>& operator -= (const Numpy<Type>&);
-	Numpy<Type>& operator *= (const Numpy<Type>&);
-	Numpy<Type>& operator /= (const Numpy<Type>&);
-	Numpy<Type>& operator *= (const Type&);
-	Numpy<Type>& operator /= (const Type&);
-	Type** dot(const Numpy<Type>&);
-	Type** T();
+    // 矩阵的运算
+    Numpy<Type>& operator += (const Numpy<Type>&);
+    Numpy<Type>& operator -= (const Numpy<Type>&);
+    Numpy<Type>& operator *= (const Numpy<Type>&);
+    Numpy<Type>& operator /= (const Numpy<Type>&);
+    Numpy<Type>& operator *= (const Type&);
+    Numpy<Type>& operator /= (const Type&);
+    Numpy<Type>& RankPlush(const Numpy<Type>&, const int number = 0);
+    Numpy<Type>& RowPlush(const Numpy<Type>&, const int number = 0);
+    Type** dot(const Numpy<Type>&);
+    Numpy<Type>& T();
+
+    // 矩阵友元运算
+    friend Type tr(const Numpy<Type>& r);
+        
+
 };
 
 #endif
